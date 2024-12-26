b53567932357faad39c5576f16a1aa95b3125ff9
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2024-12-26 16:14:16 +0900

    🐛 Analytic 컴포넌트가 한 곳에만 나타나도록 수정 (#6)
    ### 📝 작업 내용
    
    - Analytic 컴포넌트가 App.tsx에만 한번 나타나도록 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,5 @@
-import { Analytics } from '@vercel/analytics/react';
 import type { ReactNode } from 'react';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="flex flex-col max-w-[600px] m-auto">
-      {children}
-      <Analytics mode="production" />
-    </div>
-  );
+  return <div className="flex flex-col max-w-[600px] m-auto">{children}</div>;
 };
