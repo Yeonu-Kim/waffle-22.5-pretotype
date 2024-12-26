6485d48c6581b30d1538c5ca817bb6c40a69909c
Yeonu Kim <88171936+Yeonu-Kim@users.noreply.github.com>
2024-12-26 09:01:38 +0900

    ➕ vercel analytics 적용 (#4)
    ### 📝 작업 내용
    
    - vercel analytics를 적용했습니다.
    
    ### 📸 스크린샷 (선택)
    
    ### 🚀 리뷰 요구사항 (선택)
    
    - 리뷰어가 특별히 봐주었으면 하는 부분이 있다면 작성해주세요.

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { ReactNode } from 'react';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="flex flex-col max-w-[600px] m-auto">{children}</div>;
+  return (
+    <div className="flex flex-col max-w-[600px] m-auto">
+      {children}
+      <Analytics mode="production" />
+    </div>
+  );
 };
